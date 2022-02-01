28d5dd74253edc7a5695606ba607372152620b04
Qayyum Yazid <smolpadok@protonmail.com>
2022-02-02 01:24:51 +0800

    Init Next.js and Tailwind

---
+++ pages/index.js
@@ -0,0 +1,16 @@
+import Head from 'next/head'
+import Image from 'next/image'
+import styles from '../styles/Home.module.css'
+
+export default function Home() {
+  return (
+    <div className='container mx-auto text-center'>
+      <h1 className='text-3xl font-bold'>
+        Dras'kay Nirni! 
+      </h1>
+      <p className='font-bold'>
+        The mercantile engine will serve soon.
+      </p>
+    </div>
+  )
+}
